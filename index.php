858280daf0487ffe05eaf03f810f29126b4aaabd
PebblesDV <pebblesdevries@gmail.com>
2024-06-14 12:21:11 +0200

    tekst en temporary slideshow toegevoegd

--- index.php
+++ index.php
@@ -12,14 +12,30 @@
 
     <?php include "header.php" ?>
 
-    <div>
-        <div class="top-info">
-            <div class="text-box">
-                <p class="head-text">Welkom bij de website van Hendrik Hogendijk, hovenier in de regio Utrecht, Zeist en de Bilt.</p>
-                <p class="info-text">De tuin is een belangrijke plek van de woning, waar je het liefst zoveel mogelijk tijd in doorbrengt. Bij Hendrik Hogendijk Hoveniers vinden wij het daarom belangrijk dat iedereen zich thuis voelt in zijn of haar tuin. Ik maak de tuin onderdeel van jouw ‘thuis’, door hem volledig op jouw wensen af te stemmen.</p>
-            </div>
+
+    <div class="top-info">
+        <div class="text-box">
+            <p class="head-text">Welkom bij de website van Hendrik Hogendijk, hovenier in de regio Utrecht, Zeist en de Bilt.</p>
+            <p class="info-text">De tuin is een belangrijke plek van de woning, waar je het liefst zoveel mogelijk tijd in doorbrengt. Bij Hendrik Hogendijk Hoveniers vinden wij het daarom belangrijk dat iedereen zich thuis voelt in zijn of haar tuin. Ik maak de tuin onderdeel van jouw ‘thuis’, door hem volledig op jouw wensen af te stemmen.</p>
         </div>
     </div>
+
+    <div class="text-and-slideshow">
+        <div class="text-box offers-text">
+            <p class="head-text">Wat bied ik aan?</p>
+            <p class="info-text">Met behulp van mooie, natuurlijke en duurzame producten en materialen creëer ik een tuin, die garant staat voor een jarenlang plezierig buitenleven. Van een knusse veranda en een mooie vijver, tot een gezellig terras en een kleurrijke bloemenborder: ik stop al mijn energie erin.</p>
+        </div>
+
+        <!-- even temporary op deze manier zodat we wel zien hoe het wordt als het uiteindelijk werkt -->
+        <div class="slideshow">
+            <img class="slide-arrow" src="assets/arrow-left.svg" alt="arrow-left">
+            <img class="slide-img" src="assets/fotos/slide1.webp" alt="slide1">
+            <img class="slide-img" src="assets/fotos/slide2.webp" alt="slide2">
+            <img class="slide-img" src="assets/fotos/slide3.webp" alt="slide3">
+            <img class="slide-arrow" src="assets/arrow-right.svg" alt="arrow-right">
+        </div>
+    </div>
+
 
     <?php include "footer.php" ?>
 
