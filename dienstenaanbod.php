13852236b127585256420478df16d5e8acb52dc4
PebblesDV <pebblesdevries@gmail.com>
2024-06-18 13:21:45 +0200

    begonnen met dienstenaanbod

--- dienstenaanbod.php
+++ dienstenaanbod.php
@@ -4,12 +4,44 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/dienstenaanbod.css">
     <title>Dienstenaanbod</title>
 </head>
 
 <body>
 
     <?php include "header.php" ?>
+
+    <div class="full-aanbod">
+        <div class="aanbod aanbod-one">
+            <div class="text-section">
+                <p class="head-text-white">Tuinontwerp</p>
+                <p class="info-text-white">Wat jouw wensen ook zijn, ik maak een goed doordacht ontwerp voor de tuin van je dromen. Met mijn ontwerp krijg je een duidelijk beeld van jouw toekomstige tuin. Een mooie tuin valt of staat bij een goed tuinontwerp. Als het op ontwerpen aankomt maakt Hendrik Hogendijk het verschil.</p>
+            </div>
+        </div>
+
+        <div class="aanbod aanbod-two">
+            <div class="text-section text-left">
+                <p class="head-text-white">Tuinaanleg</p>
+                <p class="info-text-white">Van tuinontwerp naar jouw gewenste tuin. Wij verzorgen de aanleg met een goed voorbereid plan. Met een kritisch oog en precisie leg ik de tuin voor je aan. Het aanleggen van een tuin doen ik altijd vanuit dezelfde drive: ik wil er iets moois van maken.</p>
+            </div>
+        </div>
+
+        <div class="aanbod aanbod-three">
+            <div class="text-section">
+                <p class="head-text-white">Tuinonderhoud</p>
+                <p class="info-text-white">Om het hele jaar door optimaal te genieten van jouw tuin is het belangrijk dat deze goed onderhouden wordt. Ik neem je graag deze zorg uit handen. Een mooie tuin verdient het juiste onderhoud. Immers, goed onderhoud is een essentiële voorwaarde voor een tuin die het hele jaar door mooi blijft.</p>
+            </div>
+        </div>
+
+        <div class="aanbod aanbod-four">
+            <div class="text-section text-left">
+                <p class="head-text-white">Zwemvijvers</p>
+                <p class="info-text-white">Zwemvijvers, is één van mjn grootste passies. En gelukkig, ze winnen aan steeds meer populariteit, waardoor ik er veel ervaring mee heb opgebouwd.</p>
+            </div>
+        </div>
+    </div>
 
     <?php include "footer.php" ?>
 
