c3889b2bd9ee91634d1e8899d397ab265b80551a
PebblesDV <pebblesdevries@gmail.com>
2024-06-20 12:05:48 +0200

    begonnen met fotos op portfolio page

--- portfolio.php
+++ portfolio.php
@@ -18,6 +18,17 @@
         <p class="bekijk-dienst">Bekijk die<a class="dienstenaanbod-link" href="dienstenaanbod.php"><span>HIER</span></a>!</p>
     </div>
 
+    <div class="portfolio-imgs">
+        <!-- <div class="test bliep"></div> -->
+        <img src="assets/Images/a8db60_9f168361c12f4ad5800e1acf22f49080_mv2.jpeg" alt="img1" class="img1" width="100%" height="100%">
+        <img src="assets/Images/a8db60_e7ba32f754a8415588e6327b5647b8d6_mv2.webp" alt="img2" class="img2" width="100%" height="100%">
+        <img src="assets/Images/a8db60_87f9493ee50244389acb45a8ed75afe4_mv2.webp" alt="img3" class="img3" width="100%" height="100%">
+        <img src="assets/Images/a8db60_41e28b52fdc8461dbe9e00368c7cf8d4_mv2.jpeg" alt="img4" class="img4" width="100%" height="100%">
+        <img src="assets/Images/a8db60_379dcd1d20d04347b8fac28edbeb45c4_mv2.jpeg" alt="img5" class="img5" width="100%" height="100%">
+        <img src="assets/Images/a8db60_fbe67e59c7c94339903b8a24155922f0_mv2.jpeg" alt="img6" class="img6" width="100%" height="100%">
+        <img src="assets/Images/a8db60_8430792b04494ef1985704c9dda7049d_mv2.webp" alt="img7" class="img7" width="100%" height="100%">
+    </div>
+
     <?php include "footer.php" ?>
 
 </body>
