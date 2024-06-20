320b0541429d498421b0377b8f51df883aff9992
PebblesDV <pebblesdevries@gmail.com>
2024-06-20 09:11:57 +0200

    begonnen met portfolio page

--- portfolio.php
+++ portfolio.php
@@ -4,12 +4,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/portfolio.css">
     <title>Portfolio</title>
 </head>
 
 <body>
 
     <?php include "header.php" ?>
+
+    <div class="portfolio-text">
+        <p class="head-text">Bekijk hier een selectie van de door mij uitgevoerde diensten</p>
+        <p class="info-text">Benieuwd naar mijn volledige dienstenaanbod?</p>
+        <p class="info-text">Bekijk die<a class="dienstenaanbod-link" href="dienstenaanbod.php"><span>HIER</span></a>!</p>
+    </div>
 
     <?php include "footer.php" ?>
 
